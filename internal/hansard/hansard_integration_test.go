b17b5aa584ad563f3f01aa93d0bdbfed25958fe7
Michael Leow <leow@sinarproject.org>
2019-09-29 23:04:22 +0800

    Summary: Port over the helper function to load PDF fixture

--- internal/hansard/hansard_integration_test.go
+++ internal/hansard/hansard_integration_test.go
@@ -1,11 +1,20 @@
 package hansard_test
 
 import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/Sinar/go-dundocs/internal/hansard"
 )
+
+var updateHansard = flag.Bool("update", false, "update Hansard .golden files")
+var updateHansardPDF = flag.Bool("updatePDF", false, "update Hansard .fixture PDFs")
 
 func TestNewHansardQuestions(t *testing.T) {
 	type args struct {
@@ -18,6 +27,12 @@ func TestNewHansardQuestions(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
+		{"case #1", args{nil, nil}, false},
+		{"case #2", args{nil, nil}, false},
+		{"case #3", args{nil, nil}, false},
+		{"case #4", args{nil, nil}, false},
+		{"case #5", args{nil, nil}, false},
+		{"case #6", args{nil, nil}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -39,7 +54,12 @@ func TestNewHansardDocument(t *testing.T) {
 		want    *hansard.HansardDocument
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{"happy #1", args{"", ""}, nil, false},
+		{"happy #1", args{"", ""}, nil, false},
+		{"happy #1", args{"", ""}, nil, false},
+		{"happy #1", args{"", ""}, nil, false},
+		{"happy #1", args{"", ""}, nil, false},
+		{"happy #1", args{"", ""}, nil, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -53,4 +73,43 @@ func TestNewHansardDocument(t *testing.T) {
 			}
 		})
 	}
+}
+
+// Helper function to load from fixture; safe/update as per necessary?
+func loadPDFFromFixture(t *testing.T, fixtureLabel string, sourcePath string) *hansard.PDFDocument {
+	// Mark as helper
+	t.Helper()
+
+	var pdfDoc *hansard.PDFDocument
+	// Read from cache; if not exist; complain that need to update
+	fixture := filepath.Join("testdata", fixtureLabel+".fixture")
+	if *updateHansardPDF {
+		// If run update; call the same function used by TOC to get the data
+		pdfDoc, err := hansard.NewPDFDocument(sourcePath, nil)
+		if err != nil {
+			t.Fatalf("NewPDFDocument FAIL: %s", err.Error())
+		}
+		// Persist the data into the file
+		w, werr := yaml.Marshal(pdfDoc)
+		if werr != nil {
+			t.Fatalf("Marshal FAIL: %s", werr.Error())
+		}
+		ioutil.WriteFile(fixture, w, 0644)
+		return pdfDoc
+	}
+	// Normal path,read from fixture ,..
+	want, rerr := ioutil.ReadFile(fixture)
+	if rerr != nil {
+		// Cannot proceed with one golden file update
+		if os.IsNotExist(rerr) {
+			t.Fatalf("Ensure run with -updateHansardPDF flag first time! ERR: %s", rerr.Error())
+		}
+		t.Fatalf("Unexpected error: %s", rerr.Error())
+	}
+	pdfDoc = &hansard.PDFDocument{}
+	umerr := yaml.Unmarshal(want, pdfDoc)
+	if umerr != nil {
+		t.Fatalf("Unmarshal FAIL: %s", umerr.Error())
+	}
+	return pdfDoc
 }
