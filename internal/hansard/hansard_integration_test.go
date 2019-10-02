00cf49ea2ef05781f81aa99a2835a409640ad01f
Michael Leow <leow@sinarproject.org>
2019-10-03 03:02:40 +0800

    Summary: Add test case for NewDoc; + fixture for sampling

--- internal/hansard/hansard_integration_test.go
+++ internal/hansard/hansard_integration_test.go
@@ -5,8 +5,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 
 	"gopkg.in/yaml.v2"
 
@@ -18,7 +19,8 @@ var updateHansardPDF = flag.Bool("updateHansardPDF", false, "update Hansard .fix
 
 func TestNewHansardQuestions(t *testing.T) {
 	type args struct {
-		pdfDoc           *hansard.PDFDocument
+		fixtureLabel     string
+		pdfPath          string
 		hansardQuestions *[]hansard.HansardQuestion
 	}
 	tests := []struct {
@@ -26,19 +28,26 @@ func TestNewHansardQuestions(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
-		{"case #1", args{nil, nil}, false},
-		{"case #2", args{nil, nil}, false},
-		{"case #3", args{nil, nil}, false},
-		{"case #4", args{nil, nil}, false},
-		{"case #5", args{nil, nil}, false},
-		{"case #6", args{nil, nil}, false},
+		{"case #1", args{"HDOC-Lisan-1-20", "raw/Lisan/SOALAN MULUT (1-20).pdf", &[]hansard.HansardQuestion{{"1", 1, 4}, {"2", 5, 5}}}, false},
+		//{"case #2", args{"HDOC-Lisan-1-20", "raw/Lisan/SOALAN MULUT (1-20).pdf", nil}, false},
+		{"case #3", args{"HDOC-BukanLisan-1-20", "raw/BukanLisan/1 - 20.pdf", &[]hansard.HansardQuestion{{"1", 1, 1}, {"2", 2, 2}, {"3", 3, 5}}}, false},
+		//{"case #4", args{"HDOC-BukanLisan-1-20", "raw/BukanLisan/1 - 20.pdf", nil}, false},
+		{"sad #1", args{"HDOC-Lisan-1-20", "raw/Lisan/SOALAN MULUT (1-20).pdfa", nil}, true},
+		{"sad #2", args{"HDOC-BukanLisan-1-20", "raw/BukanLisan/1 - 20b.pdf", nil}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := hansard.NewHansardQuestions(tt.args.pdfDoc, tt.args.hansardQuestions); (err != nil) != tt.wantErr {
+			pdfDoc := samplePDFFromFixture(t, tt.args.fixtureLabel, tt.args.pdfPath)
+			hansardQuestions := make([]hansard.HansardQuestion, 0, 20)
+			if err := hansard.NewHansardQuestions(pdfDoc, &hansardQuestions); (err != nil) != tt.wantErr {
 				t.Errorf("NewHansardQuestions() error = %v, wantErr %v", err, tt.wantErr)
 			}
+
+			// Show diff if any ..
+			if diff := cmp.Diff(tt.args.hansardQuestions, &hansardQuestions); diff != "" {
+				t.Errorf("hansardQuestions mismatch (-want +got):\n%s", diff)
+			}
+
 		})
 	}
 }
@@ -71,11 +80,54 @@ func TestNewHansardDocument(t *testing.T) {
 			//	t.Errorf("NewHansardDocument() error = %v, wantErr %v", err, tt.wantErr)
 			//	return
 			//}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewHansardDocument() got = %v, want %v", got, tt.want)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("NewHansardDocumentContent() mismatch (-want +got):\n%s", diff)
 			}
+
+			//if !reflect.DeepEqual(got, tt.want) {
+			//	t.Errorf("NewHansardDocument() got = %v, want %v", got, tt.want)
+			//}
 		})
 	}
+}
+
+// Helper function to sample 5 pages from fixture
+func samplePDFFromFixture(t *testing.T, fixtureLabel string, sourcePath string) *hansard.PDFDocument {
+	// Mark as helper
+	t.Helper()
+
+	var pdfDoc *hansard.PDFDocument
+	// Read from cache; if not exist; complain that need to update
+	fixture := filepath.Join("testdata", "sample-"+fixtureLabel+".fixture")
+	if *updateHansardPDF {
+		// If run update; call the same function used by TOC to get the data
+		pdfDoc, err := hansard.NewPDFDocument(sourcePath, &hansard.ExtractPDFOptions{NumPages: 5})
+		if err != nil {
+			t.Fatalf("NewPDFDocument FAIL: %s", err.Error())
+		}
+		// Persist the data into the file
+		w, werr := yaml.Marshal(pdfDoc)
+		if werr != nil {
+			t.Fatalf("Marshal FAIL: %s", werr.Error())
+		}
+		ioutil.WriteFile(fixture, w, 0644)
+		return pdfDoc
+	}
+	// Normal path,read from fixture ,..
+	want, rerr := ioutil.ReadFile(fixture)
+	if rerr != nil {
+		// Cannot proceed with one golden file update
+		if os.IsNotExist(rerr) {
+			t.Fatalf("Ensure run with -updateHansardPDF flag first time! ERR: %s", rerr.Error())
+		}
+		t.Fatalf("Unexpected error: %s", rerr.Error())
+	}
+	pdfDoc = &hansard.PDFDocument{}
+	umerr := yaml.Unmarshal(want, pdfDoc)
+	if umerr != nil {
+		t.Fatalf("Unmarshal FAIL: %s", umerr.Error())
+	}
+	return pdfDoc
 }
 
 // Helper function to load from fixture; safe/update as per necessary?
